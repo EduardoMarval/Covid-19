c0d4dc4e66ae61619e8ffe516307cd9e27a9a76d
EduardoMarval <emarvalr@gmail.com>
2022-04-19 20:06:58 -0400

    Login Beta(1.2)
    Split JS

--- public/covid19/assets/js/scripts.js
+++ public/covid19/assets/js/scripts.js
@@ -1,11 +1,10 @@
 //Declaramos las constantes y variables globales
-const datos_tabla = document.getElementById("datos_tabla");
-const modalCuerpo = document.getElementById("modalCuerpo");
 const pais = document.getElementById("pais");
-const itemSesion = document.getElementById("item-sesion");
-const situacionChile = document.getElementById("situacionChile");
-const errorLogin = document.getElementById("errorLogin");
 const splitarray = [];
+let increaser = '0';
+let next = document.getElementById("next-page");
+let previous = document.getElementById("previous-page");
+let myChart;
 const spacesArray = [
   {
     location: "GB",
@@ -255,66 +254,6 @@ const spacesArray = [
     active: 0,
   },
 ];
-
-let increaser = '0';
-let next = document.getElementById("next-page");
-let previous = document.getElementById("previous-page");
-let myChart;
-
-//llamada a la funcion del login
-$('#js-form').submit(async (event) => {
-  event.preventDefault()
-  const email = document.getElementById('js-input-email').value
-  const password = document.getElementById('js-input-password').value
-  const JWT = await postData(email, password)
-  const posts = await getPosts(JWT)
-  fillTable(posts, 'js-table-posts')
-})
-
-//funcion para traer el token del login
-const postData = async (email, password) => {
-  try {
-    const response = await fetch('http://localhost:3000/api/login',
-      {
-        method: 'POST',
-        body: JSON.stringify({ email: email, password: password })
-      })
-
-    const { token } = await response.json()
-    
-    if (token === undefined) {
-      errorLogin.innerHTML = `<p class="error-form text-center animate__animated animate__shakeX">Datos incorrectos</p>`;
-      //loading.classList.add("remove");
-    } else {
-      localStorage.setItem("MasterKey", token);
-
-      errorLogin.innerHTML = "";
-      location.reload();
-    }
-
-    return token;
-  } catch (err) {
-    console.error(`Error: ${err} `)
-  }
-}
-
-
-(() => {
-  const cerrarSesion = document.getElementById("cerrarSesion");
-
-  if (localStorage.getItem("MasterKey") != undefined) {
-    itemSesion.innerHTML = `
-    <a id="cerrarSesion" class="nav-link" href="#" >Cerrar Sesión</a>`;
-    situacionChile.innerHTML =
-      '<a class="nav-link situacion" href="/covid19/situacion-chile.html">Situación en Chile</a>';
-  } else {
-    itemSesion.innerHTML = `
-    <a id="nav-item-login" class="nav-link" href="#"  data-toggle="modal" data-target="#loginModal">Iniciar Sesión</a>`;
-    situacionChile.innerHTML = "";
-  }
-})();
-
-
 
 //conexion con la API
 const getTotalData = async () => {
@@ -592,15 +531,3 @@ const getSingleData = async (locationParameter) => {
     console.log(err);
   }
 };
-
-cerrarSesion.addEventListener("click", async (e) => {
-  localStorage.removeItem("MasterKey");
-  window.location.href = "/covid19/";
-});
-
-
-loginButton.addEventListener("click", async (e) => {
-  e.preventDefault();
-  loginValidation(userEmail.value, userPassword.value);
-});
-
